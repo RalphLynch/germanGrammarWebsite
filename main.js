04d36bb7b0911e535b00bf507d7d495b15c06b3b
Ralph Lynch <ralphlynch1@protonmail.com>
2021-06-30 18:04:21 +0100

    Post method works out name of exercise

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ app.use(express.static('website'));
 app.listen(port, () => {
     console.log(`Website listening at http://localhost:${port}`);
 })
+
+app.post('/checkAnswers', (req, res) => {
+    let exerciseName = req.header('Referer').split("/").pop().slice(0, -5);
+})
