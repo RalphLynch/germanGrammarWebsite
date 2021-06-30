c851ca25473f468f92b5ded463a5a8adb2b4da99
Ralph Lynch <ralphlynch1@protonmail.com>
2021-06-30 18:07:48 +0100

    Added comments to main.js

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Loads the static HTML files for the website
 app.use(express.static('website'));
 
 app.listen(port, () => {
+    // Listens for anyone connecting to the website
     console.log(`Website listening at http://localhost:${port}`);
 })
 
 app.post('/checkAnswers', (req, res) => {
+    // Extracts the name of the exercise from the Referer HTTP header
     let exerciseName = req.header('Referer').split("/").pop().slice(0, -5);
 })
