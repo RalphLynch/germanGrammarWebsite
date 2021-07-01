e67a07c76a95d1987da9cdd1d8f6c575b12fe3ce
Ralph Lynch <06504@farnborough.ac.uk>
2021-07-01 09:35:28 +0100

    Added loop to check answers and calculate score

--- main.js
+++ main.js
@@ -17,10 +17,16 @@ app.post('/checkAnswers', (req, res) => {
     // Extracts the name of the exercise from the Referer HTTP header
     let exerciseName = req.header('Referer').split("/").pop().slice(0, -5);
 
-    console.log(req.body);
-
     // Reads the solutions to the questions from a JSON file
     let solutionsObject = JSON.parse(fs.readFileSync(__dirname + `/solutions/${exerciseName}Solutions.json`));
 
+    // Checks the answers and adds up the score
     var score = 0;
+    var i = 0
+    for (let key in req.body) {
+        if (req.body[key] === solutionsObject.solutions[i]) {
+            score++;
+        }
+        i++
+    }
 })
