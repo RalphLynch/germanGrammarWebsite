714a44b0c2ed5d2250ef77d969b655965ac5dc08
Ralph Lynch <06504@farnborough.ac.uk>
2021-07-01 11:40:32 +0100

    Added comment

--- website/script.js
+++ website/script.js
@@ -8,6 +8,7 @@ function loadQuestions(exercise) {
     .then(data => {
         // Inserts each question into an ordered list
         for (let i = 0; i < data.questions.length; i++) {
+            // 1 is added to i because the ids are off by one.
             document.getElementById(`question${i + 1}`).innerHTML = data.questions[i];
         }
     })
