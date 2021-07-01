a96659cd81efcc003623d3cc5beb9a10198eff34
Ralph Lynch <06504@farnborough.ac.uk>
2021-07-01 10:17:12 +0100

    Added comments

--- website/script.js
+++ website/script.js
@@ -1,9 +1,12 @@
 function loadQuestions(exercise) {
+    // Generates the URL for the file containing the questions
     const questions = new Request(`http://localhost:3000/${exercise}Questions.json`);
 
+    // Fetches the question file
     fetch(questions)
     .then(response => response.json())
     .then(data => {
+        // Inserts each question into an ordered list
         for (let i = 0; i < data.questions.length; i++) {
             document.getElementById(`question${i + 1}`).innerHTML = data.questions[i];
         }
