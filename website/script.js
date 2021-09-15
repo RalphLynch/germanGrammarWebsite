335370ee01e2f9f77aa279f3d5ab3ae2673ab8c0
Ralph Lynch <06504@farnborough.ac.uk>
2021-09-15 15:19:56 +0100

    Questions are now randomly selected

--- website/script.js
+++ website/script.js
@@ -3,7 +3,7 @@ function loadQuestions(exercise) {
     const questions = new Request(`http://localhost:3000/${exercise}Questions.json`);
 
     // Stores questions that have been selected to be displayed
-    let selectedQuestions = [];
+    let selectedQuestions = new Array(0);
 
     // Fetches the question file
     fetch(questions)
@@ -11,9 +11,20 @@ function loadQuestions(exercise) {
     .then(response => response.json())
     .then(data => {
         // Inserts each question into an ordered list
-        for (let i = 0; i < data.questions.length; i++) {
-            // 1 is added to i because the ids are off by one.
-            document.getElementById(`question${i + 1}`).innerHTML = data.questions[i];
+        for (let i = 0; i < 5; i++) {
+            // Selects a random index of a question
+            let questionIndex = Math.floor(Math.random() * 5);
+
+            // If the index has already been selected, a new index is chosen until it is a new index
+            while (selectedQuestions.includes(questionIndex)) {
+                questionIndex = Math.floor(Math.random() * 5);
+            }
+
+            // Inserts the question into an ordered list
+            document.getElementById(`question${i + 1}`).innerHTML = data.questions[questionIndex];
+
+            // Adds the index of the selected question to an array.
+            selectedQuestions.push(questionIndex);
         }
     })
 }
